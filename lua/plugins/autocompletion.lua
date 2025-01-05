9dfaacf6c71c2bd0ee34bea15c8b7716e74ef40d
CameronBadman <cbadwork@gmail.com>
2025-01-05 23:17:07 +1000

    fixed code snippets menu to use alt-j, alt-k, and tab for commands

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -67,20 +67,15 @@ cmp.setup({
 		end,
 	},
 	mapping = {
-		-- Let's try normal keys that work everywhere
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<A-k>"] = cmp.mapping.select_prev_item(),
+		["<A-j>"] = cmp.mapping.select_next_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
-		["<CR>"] = cmp.mapping.confirm({
-			select = true,
-			behavior = cmp.ConfirmBehavior.Replace,
-		}),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
-				cmp.select_next_item()
+				cmp.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
 			else
